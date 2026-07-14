e60c6069967cc6322871f2d8f51030c951cc658a
Niphram <theniphram@gmail.com>
2026-07-14 10:52:15 +0200

    add adguard dns

---
+++ cluster/Argcfile.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+# @meta dotenv .env
+
+# @cmd
+# @option --talosversion! $TALOS_VERSION <version> bind-env
+# @option --k8sversion! $KUBERNETES_VERSION <version> bind-env
+# @option --cluster! $CLUSTER_NAME <name> bind-env
+gen-config() {
+    echo "Generating talos config for cluster $argc_cluster..."
+
+    PATCHES=""
+    for file in ./patches/common/*.yaml
+    do
+        echo "Applying common patch $file"
+        PATCHES="${PATCHES} --config-patch @$file"
+    done
+
+    for file in ./patches/control-plane/*.yaml
+    do
+        echo "Applying control-plane patch $file"
+        PATCHES="${PATCHES} --config-patch-control-plane @$file"
+    done
+
+    for file in ./patches/worker/*.yaml
+    do
+        echo "Applying worker patch $file"
+        PATCHES="${PATCHES} --config-patch-worker @$file"
+    done
+
+    talosctl gen config --force -o ./generated/ \
+        --kubernetes-version ${argc_k8sversion} \
+        --talos-version ${argc_talosversion} \
+        --with-secrets secrets.yaml \
+        ${PATCHES} \
+        ${argc_cluster} "https://192.168.178.200"
+}
+
+eval "$(argc --argc-eval "$0" "$@")"
